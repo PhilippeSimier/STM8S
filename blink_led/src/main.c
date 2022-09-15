96e022fb4a3c2abfb032df4d8ed059cb9bf24f09
philippeSimier <philaure@wanadoo.fr>
2022-09-15 17:47:33 +0200

    improve blink led builtin

--- blink_led/src/main.c
+++ blink_led/src/main.c
@@ -6,10 +6,10 @@
  */
 
 
-#include <stm8s.h>
+#include <stm8s_conf.h>
 
-#define LED_GPIO_PORT  (GPIOC)
-#define LED_GPIO_PINS  (GPIO_PIN_5)
+#define LED_GPIO_PORT  GPIOC
+#define LED_GPIO_PIN   (GPIO_Pin_TypeDef)GPIO_PIN_5
 
 // Prototypes
 void Delay(uint16_t nCount);
@@ -17,20 +17,27 @@ void Delay(uint16_t nCount);
 void main() {
 
     /* Setup  Initialize GPIO */
-    GPIO_Init(LED_GPIO_PORT, (GPIO_Pin_TypeDef) LED_GPIO_PINS, GPIO_MODE_OUT_PP_LOW_FAST);
+    GPIO_Init(LED_GPIO_PORT,  LED_GPIO_PIN, GPIO_MODE_OUT_PP_LOW_FAST);
+ 
 
     /* Loop */
     while (1) {
 
-        GPIO_WriteReverse(LED_GPIO_PORT, (GPIO_Pin_TypeDef) LED_GPIO_PINS); // Toggles Led builtin
-        Delay(0xFFFF);
-
+        GPIO_WriteHigh(LED_GPIO_PORT,  LED_GPIO_PIN);
+        Delay(2);
+        GPIO_WriteLow(LED_GPIO_PORT,   LED_GPIO_PIN);
+        Delay(10);
     }
 
 }
 
 void Delay(uint16_t nCount) {
+    uint16_t n = 0xFFFF;
     while (nCount != 0) {
+        n = 0xFFFF;
+        while (n != 0){
+            n--;
+        }
         nCount--;
     }
 }
@@ -41,11 +48,10 @@ void Delay(uint16_t nCount) {
  * @param file: pointer to the source file name
  * @param line: assert_param error line source number
  * @retval None
- */
+*/ 
 
 void assert_failed(uint8_t* file, uint32_t line) {
-    
-
+  
     // Infinite loop 
     while (1) {
     }
