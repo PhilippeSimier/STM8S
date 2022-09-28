3a8843afe3c9da2ee117507df8ce320b211869d2
philippeSimier <philaure@wanadoo.fr>
2022-09-28 12:47:15 +0200

    add timer

---
+++ timer/README.md
@@ -0,0 +1,37 @@
+﻿# STM8 Timer
+
+## Calcul de la période
+
+Les temporisateurs à l'intérieur des microcontrôleurs STM8 sont des périphériques très utiles et essentiels pour les applications de temporisation.
+
+Le STM8S possède un oscillateur interne avec une fréquence de 16 MHz. Par défaut, il sera utilisé comme source d'horloge système avec le prédiviseur 1, ce qui signifie que la fréquence d'horloge système sera de **16 MHz**. 
+
+Par défaut, le timer 2 fonctionnera avec la même fréquence que le système.
+Supposons que nous voulions basculer la LED à chaque seconde et que nous allions utiliser la minuterie 2 pour identifier le moment où 1 seconde s'est écoulée.
+
+Chaque tick de l'horloge système sera 1/16 000 000 = 0,0000000625 seconde ou **0,0625 µs**.
+
+Si nous définissons 2048 comme prédiviseur pour le temporisateur 2, le compteur du temporisateur 2 s'incrémentera toutes les **128 µs** (0,0625 µs x 2048).
+
+Ainsi, 1 seconde se seront écoulé lorsque le compteur du timer atteindra  1 000 000/128 = **7812** .
+
+Le timer 2 a un compteur de 16 bits.
+
+## Programme
+```c
+/**
+ * @brief Active l'horloge du timer 2
+ *      Configure le pré-diviseur, sur 2048
+ *      Configure le retour à zero du compteur sur la valeur nb
+ *      la periode d'une seconde correspond à 7812
+ * @param nb la valeur maxi du compteur
+ */
+void TIM2_setup(void) {
+	CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER2, ENABLE);
+    TIM2_DeInit();
+    TIM2_TimeBaseInit(TIM2_PRESCALER_2048, 7812); 
+    TIM2_Cmd(ENABLE);
+}
+```
+
+
--- timer/src/main.c
+++ timer/src/main.c
@@ -14,21 +14,18 @@
 #define ENABLE true
 #define DISABLE false
 
-
-
-
 int state = 0;
 
 void GPIO_setup(void);
 void clock_setup(void);
-void TIM2_setup(void);
+void TIM2_setup(uint16_t nb);
 
 
 void main(void) {
 
     clock_setup();
     GPIO_setup();
-    TIM2_setup();
+    TIM2_setup(7048); 
 
     begin(115200);
   
@@ -36,11 +33,11 @@ void main(void) {
     bool flag = false;
 
    
-    //la led s'allume et s'éteint toutes les 125ms  1952/2=976
+    //la led s'allume et s'éteint toutes les 500ms  7812/2=3906
     while (1) {
         
         //test du timer
-        if (TIM2_GetCounter() > 976) { 
+        if (TIM2_GetCounter() > 3906) { 
             if (flag == false) {
                 GPIO_WriteHigh(PORTC, LED_BUILTIN);
                 printf(" Led on !\r\n");
@@ -57,16 +54,17 @@ void main(void) {
 }
 
 
-
+/**
+ * fonction pour définir la broche PB5 comme sortie.
+ * Output push-pull, low level, 10MHz
+ */
 void GPIO_setup(void) {
-
-//    GPIO_DeInit(GPIOA);
-//    GPIO_DeInit(GPIOB);
-//    GPIO_DeInit(GPIOD);
-    GPIO_Init(PORTC, LED_BUILTIN, GPIO_MODE_OUT_PP_LOW_FAST); // Output push-pull, low level, 10MHz
+    GPIO_Init(PORTC, LED_BUILTIN, GPIO_MODE_OUT_PP_LOW_FAST); 
 }
 
-
+/**
+ * Fonction pour configurer l'oscillateur interne (16Mhz)
+ */
 void clock_setup(void) {
     CLK_DeInit();
 
@@ -78,36 +76,25 @@ void clock_setup(void) {
     CLK_ClockSwitchCmd(ENABLE);
     
 
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
-    //CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV8); //un peu lent
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);   // HSI à 16 MHz
+    //CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV8); // HSI à  2 MHz
 
-    CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1); //soit 16Mhz
-
-    //CLK_ClockSwitchConfig(CLK_SWITCHMODE_AUTO, CLK_SOURCE_HSI,  
-    //DISABLE, CLK_CURRENTCLOCKSTATE_ENABLE); 
-
-    CLK_PeripheralClockConfig(CLK_PERIPHERAL_SPI, DISABLE);
-    CLK_PeripheralClockConfig(CLK_PERIPHERAL_I2C, DISABLE);
-    CLK_PeripheralClockConfig(CLK_PERIPHERAL_ADC, DISABLE);
-    CLK_PeripheralClockConfig(CLK_PERIPHERAL_AWU, DISABLE);
-    CLK_PeripheralClockConfig(CLK_PERIPHERAL_UART3, ENABLE);
-    CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER1, DISABLE);
-    CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER2, ENABLE);
-    CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER4, DISABLE);
+    CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1); // soit CPU à 16Mhz
 }
 
 
-/*
- l'horloge qui rentre dans le timer = fmaster = 16Mhz
- préscaler = 2048 donc la fréquence d'entrée du timer est 16Mhz/2048 soit 7812,5Hz
- la valeur max de comptage est 1952, après le compteur se réinitialse à zéro et recommence.
- donc la fréquence de rechargement est 7812,5/1952=4,002305328 HZ
- soit une période de 1/4=250ms
- 
+
+/**
+ * @brief Active l'horloge du timer 2
+ *      Configure le pré-diviseur, sur 2048
+ *      Configure le retour à zero du compteur sur la valeur nb
+ *      la periode d'une seconde correspond à 7812
+ * @param nb la valeur maxi du compteur
  */
-void TIM2_setup(void) {
-    TIM2_DeInit();
-    TIM2_TimeBaseInit(TIM2_PRESCALER_2048, 1952); //soit 16Mhz/2048=
+void TIM2_setup(uint16_t nb) {
+    CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER2, ENABLE);
+    TIM2_DeInit();    
+    TIM2_TimeBaseInit(TIM2_PRESCALER_2048, nb); 
     TIM2_Cmd(ENABLE);
 }
 
