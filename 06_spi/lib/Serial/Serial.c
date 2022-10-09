dd1c03a96b976f79174427b43caf1ef4648b58f2
philippeSimier <philaure@wanadoo.fr>
2022-10-09 14:11:40 +0200

    add SPI

---
+++ 06_spi/lib/Serial/Serial.c
@@ -0,0 +1,95 @@
+#include "Serial.h"
+
+/**
+ * @brief initialise l'uart3 
+ * @param baudrate Le taux de transmission
+ */
+void begin(uint32_t baudrate) {
+
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV8);
+
+    UART3_DeInit();
+    UART3_Init(baudrate,
+            UART3_WORDLENGTH_8D,
+            UART3_STOPBITS_1,
+            UART3_PARITY_NO,
+            UART3_MODE_TXRX_ENABLE);
+}
+
+/**
+ * @brief Retargets the C library printf function to the UART3.
+ * @param c Character to send
+ * @retval char Character sent
+ */
+int putchar(int c) {
+    /* Write a character to the UART3 */
+    UART3_SendData8(c);
+    /* Loop until the end of transmission */
+    while (UART3_GetFlagStatus(UART3_FLAG_TXE) == RESET);
+
+    return (c);
+}
+
+/**
+ * @brief Retargets the C library scanf function to the USART3.
+ * @param None
+ * @retval char Character to Read
+ */
+int getchar(void) {
+    int c = 0;
+
+    /* Loop until the Read data register flag is SET */
+    while (UART3_GetFlagStatus(UART3_FLAG_RXNE) == RESET); /* wait until data arrived */
+    c = UART3_ReceiveData8();
+    return (c);
+}
+
+
+/* @Brief  :   Delay function
+ * @Param  :   Time to delay (millis seconds)
+ * @Return :   None
+ * @Note   :   None
+ */
+void delay_ms(uint32_t nb) {
+
+    for (uint32_t i = 0; i < nb; i++) {
+        // simple wait ~1ms @ 16MHz
+        for (uint32_t j = 0; j < 1600L; j++)
+            __asm__("nop");
+    }
+}
+
+/* @Brief  :   Delay function
+ * @Param  :   Time to delay (micro seconds)
+ * @Return :   None
+ * @Note   :   None
+ */
+void delay_us(uint32_t nb) {
+
+    for (uint32_t i = 0; i < nb; i++) {
+        // simple wait ~1us @ 16MHz  //pas sur à la louche
+            __asm__("nop");
+    }
+}
+
+
+
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *   where the assert_param error has occurred.
+ * @param file: pointer to the source file name
+ * @param line: assert_param error line source number
+ * @retval None
+ */
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line) {
+    /* User can add his own implementation to report the file name and line number,
+       ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+    printf("assert_failed file %s", file);
+    printf("assert_failed file %d", line);
+    while (1) {
+    }
+}
+#endif
+
---
+++ 06_spi/src/main.c
@@ -0,0 +1,124 @@
+/*
+ * Test du bus  SPI
+ * 
+ */
+#include <stm8s_conf.h>
+#include <Serial.h>
+#include <stdbool.h>
+
+#define CS_pin   GPIO_PIN_4
+#define CS_port  GPIOC
+
+void GPIO_setup();
+void clock_setup();
+
+void SPI_setup();
+void spi_write(unsigned char address, unsigned char value);
+
+
+void main(void) {
+    
+    uint16_t i = 0;
+
+    clock_setup();
+    GPIO_setup();
+    
+    begin(115200);
+
+    printf("\r\n Programme test SPI\r\n");   
+    SPI_setup();
+    
+    while (1) {
+        //test du spi
+        spi_write(0xa0, 0x36);
+        delay_ms(100);
+        printf("\r\n send %d\r\n", i++);
+    }
+}
+
+void GPIO_setup()
+ {
+    //GPIO_PinRemapConfig();
+    //GPIO_PinRemapConfig(GPIO_Remap_I2C1, ENABLE) ; 
+    GPIO_DeInit(GPIOA);
+    GPIO_DeInit(GPIOB);
+    GPIO_DeInit(GPIOC);
+    GPIO_DeInit(GPIOD);
+    
+    GPIO_Init(GPIOC, ((GPIO_Pin_TypeDef)GPIO_PIN_5 | GPIO_PIN_6),GPIO_MODE_OUT_PP_HIGH_FAST);  // PC5 -> led et PC6 -> MOSI
+    GPIO_Init(CS_port, CS_pin, GPIO_MODE_OUT_PP_HIGH_FAST); // CS  -> PC4
+    GPIO_Init(GPIOC, GPIO_PIN_7, GPIO_MODE_IN_PU_NO_IT);    // PC7 -> MISO 
+}
+
+void clock_setup() 
+{ 
+      CLK_DeInit(); 
+  
+      CLK_HSECmd(DISABLE); 
+      CLK_LSICmd(DISABLE); 
+      CLK_HSICmd(ENABLE); 
+      while(CLK_GetFlagStatus(CLK_FLAG_HSIRDY) == FALSE); 
+  
+      CLK_ClockSwitchCmd(ENABLE); 
+      //CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV8); //un peu lent
+       
+      CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
+    
+      CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1); //soit 16Mhz
+  
+      //CLK_ClockSwitchConfig(CLK_SWITCHMODE_AUTO, CLK_SOURCE_HSI,  
+      //DISABLE, CLK_CURRENTCLOCKSTATE_ENABLE); 
+  
+      CLK_PeripheralClockConfig(CLK_PERIPHERAL_SPI, ENABLE); 
+      CLK_PeripheralClockConfig(CLK_PERIPHERAL_I2C, DISABLE); 
+      CLK_PeripheralClockConfig(CLK_PERIPHERAL_ADC, DISABLE); 
+      CLK_PeripheralClockConfig(CLK_PERIPHERAL_AWU, DISABLE); 
+      CLK_PeripheralClockConfig(CLK_PERIPHERAL_UART3, ENABLE); 
+      CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER1, DISABLE); 
+      CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER2, DISABLE); 
+      CLK_PeripheralClockConfig(CLK_PERIPHERAL_TIMER4, DISABLE); 
+}
+
+void SPI_setup() {
+    
+    // plantage via la SPL
+    
+    SPI_DeInit();
+    /**
+    SPI_Init(SPI_FIRSTBIT_MSB,            envoie d'abord MSB
+            SPI_BAUDRATEPRESCALER_64,
+            SPI_MODE_MASTER,              SPI fonctionne en mode master
+            SPI_CLOCKPOLARITY_LOW,
+            SPI_CLOCKPHASE_1EDGE,
+            SPI_DATADIRECTION_1LINE_TX,    // Transmit only in 1 line bi-directional data mode 
+            SPI_NSS_HARD,                  ou SPI_NSS_SOFT
+            0x00);
+    printf("->ok2\n");
+    SPI_Cmd(ENABLE);
+    */
+    //donc init manuelle
+    SPI->CR1 = (SPI_FIRSTBIT_MSB | SPI_BAUDRATEPRESCALER_128 | SPI_MODE_MASTER | SPI_CLOCKPOLARITY_LOW | SPI_CLOCKPHASE_1EDGE);
+    SPI->CR2 = (SPI_DATADIRECTION_1LINE_TX | SPI_NSS_HARD);
+    SPI->ICR=0;
+    SPI_Cmd(ENABLE);
+    
+    printf("SPI-> CR1:%x CR2:%x\n\r",SPI->CR1,SPI->CR2); //vérification des deux registres de config
+}
+
+void spi_write(unsigned char address, unsigned char value) {
+     
+    while (SPI_GetFlagStatus(SPI_FLAG_BSY)); //attends que le bus soit pret
+    GPIO_WriteLow(CS_port, CS_pin);     //chip select
+    
+    SPI_SendData(address);              //envoi l'adresse
+    while (!SPI_GetFlagStatus(SPI_FLAG_TXE)); //attends que le registre d'envoi prenne en compte l'octet d'adresse  
+    
+    SPI_SendData(value);                      //envoi la donnée
+    while (!SPI_GetFlagStatus(SPI_FLAG_TXE)); //attends que le registre d'envoi prenne en compte l'octet de donnée
+        
+    while (SPI_GetFlagStatus(SPI_FLAG_BSY)); //attends que le bus soit libéré
+    GPIO_WriteHigh(CS_port, CS_pin); //relache le CS
+    
+}
+
+
